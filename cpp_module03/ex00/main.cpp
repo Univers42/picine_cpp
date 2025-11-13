1a72e38fa1b0d2112ca791866e7e8515f7dba63b
Dylan Lesieur <dlesieur@c2r1s6.42madrid.com>
2025-11-14 00:36:00 +0100

    working on a framework test to test more quickly  the programs

--- cpp_module03/ex00/main.cpp
+++ cpp_module03/ex00/main.cpp
@@ -6,18 +6,51 @@
 /*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 15:55:36 by dlesieur          #+#    #+#             */
-/*   Updated: 2025/11/13 15:57:37 by dlesieur         ###   ########.fr       */
+/*   Updated: 2025/11/13 23:02:00 by dlesieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
+#include "ClapTrap.hpp"
+#include <iostream>
+
+
+#include "ClapTrap.hpp"
+#include <iostream>
+
 int main()
 {
-    ClapTrap a("alpha");
-    ClapTrap b = a;
-    ClapTrap c = std::move(a);   //moved
-    b = c;                      //assigned(copy)
-    c = ClapTrap("alpha");            // assigned (move)
-    return (0);   
+    std::cout << "=== Construction ===" << std::endl;
+    ClapTrap a("Alpha");
+    ClapTrap b("Bravo");
+
+    std::cout << "\n=== Copy Construction ===" << std::endl;
+    ClapTrap c(b);
+
+    std::cout << "\n=== Move Construction ===" << std::endl;
+    ClapTrap d(std::move(a));
+
+    std::cout << "\n=== Copy Assignment ===" << std::endl;
+    c = b;
+
+    std::cout << "\n=== Move Assignment ===" << std::endl;
+    d = ClapTrap("Delta");
+
+    std::cout << "\n=== Gameplay ===" << std::endl;
+    b.attack("Delta");
+    d.takeDamage(0);
+
+    d.attack("Bravo");
+    b.takeDamage(0);
+
+    b.beRepaired(3);
+    d.beRepaired(2);
+
+    std::cout << "\n=== Damage and Repair Edge Cases ===" << std::endl;
+    b.takeDamage(20); // Should drop to 0 HP
+    b.beRepaired(5);  // Should still work if your logic allows
+
+    std::cout << "\n=== End of Test ===" << std::endl;
+    return 0;
 }

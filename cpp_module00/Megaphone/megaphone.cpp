5fac7a694bef1f3821ce36b4bfa8323a8442897b
LESdylan <dev.pro.photo@gmail.com>
2026-01-28 19:17:11 +0100

    added a norminette strict to be compliant with good practices and version I'm working on

--- cpp_module00/Megaphone/megaphone.cpp
+++ cpp_module00/Megaphone/megaphone.cpp
@@ -6,25 +6,34 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 16:05:43 by marvin            #+#    #+#             */
-/*   Updated: 2025/12/23 21:54:02 by marvin           ###   ########.fr       */
+/*   Updated: 2026/01/28 18:16:16 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libcpp.h"
+#include <cctype>
+#include <cstring>
+#include <iostream>
+#include <string>
 
-int main(int argc, char **argv) {
-  const char *fb = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-  std::string out;
+static void print_line(const char* s) { std::cout << s << '\n'; }
+static void print_line(const std::string& s) { std::cout << s << '\n'; }
+
+int main(int argc, char** argv) {
+  const char* fb = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+  char* s;
   bool first = true;
+  unsigned char ch;
 
-  if (argc == 1)
-    return (print_line(fb), 0);
-  for (char **arg = argv + 1; *arg != NULL; ++arg) {
-    std::string temp(*arg);
-    toupperstr(&temp);
-    if (!first) out.append(" ");
-    out.append(temp);
+  if (argc <= 1) return (print_line(fb), 0);
+  std::string out;
+  for (int i = 1; i < argc; ++i) {
+    if (!first) out.push_back(' ');
     first = false;
+    s = argv[i];
+    for (std::size_t j = 0; s[j] != '\0'; ++j) {
+      ch = static_cast<unsigned char>(s[j]);
+      out.push_back(static_cast<char>(std::toupper(ch)));
+    }
   }
   return (print_line(out), 0);
 }
